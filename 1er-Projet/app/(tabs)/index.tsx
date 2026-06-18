0b1c6ab00ed0ee4f01d91d0cdf95d1c00fc588b0
SamuelMorisson <morissonsamuel@gmail.com>
2026-06-18 10:05:25 +0200

    Exercices jour 1

---
+++ 1er-Projet/app/(tabs)/index.tsx
@@ -0,0 +1,27 @@
+/*
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+export default function HomeScreen() {
+ return (
+ <SafeAreaView style={styles.container}>
+ <Text>Hello</Text>
+ </SafeAreaView>
+ );
+}
+const styles = StyleSheet.create({
+ container: { flex: 1, backgroundColor: '#FFFFFF' },
+});
+
+*/
+
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <></>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, backgroundColor: '#FFFFFF'},
+});
